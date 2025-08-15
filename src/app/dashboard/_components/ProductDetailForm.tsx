327e13a2c038242d8b2cb9ecee17ff955f51a6cf
Priyanshu <sidarsunita2@gmail.com>
2025-08-15 13:22:51 +0530

    dashboard form ui done

--- src/app/dashboard/_components/ProductDetailForm.tsx
+++ src/app/dashboard/_components/ProductDetailForm.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -6,22 +6,29 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 const productDetailSchema = z.object({
   name: z.string(),
-  description: z.string(),
+  url: z.string(),
+  description: z.string().optional(),
 });
 
 export const ProductDetailForm = () => {
   const form = useForm<z.infer<typeof productDetailSchema>>({
     resolver: zodResolver(productDetailSchema),
-    defaultValues: {},
+    defaultValues: {
+      name:"",
+      url:"",
+      description:""
+    },
   });
 
   function onSubmit(values: z.infer<typeof productDetailSchema>) {
@@ -35,19 +42,56 @@ export const ProductDetailForm = () => {
         onSubmit={form.handleSubmit(onSubmit)}
         className="flex gap-6 flex-col"
       >
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Product Name</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <div className="grid gap-6 grid-cols-1 lg:grid-cols-2">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Product Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription></FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="url"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Enter Your Website URL </FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription>
+                  Include the protocol (http/https) and the full path to the
+                  sales page
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Product Description</FormLabel>
+                <FormControl>
+                  <Textarea {...field} />
+                </FormControl>
+                <FormDescription>
+                  An optional description to help distinguish your product from
+                  other products
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
       </form>
     </Form>
   );
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,9 +3,9 @@ import { Navbar } from "./_components/Navbar";
 
 export default function Dashboard({children}:{children:ReactNode}){
     return (
-        <div className="bg-accent/5 min-h-screen">
+        <div className="bg-accent/15 min-h-screen">
             <Navbar/>
-            <div className="py-6">
+            <div className="container py-6">
                 {children}
             </div>
         </div>
