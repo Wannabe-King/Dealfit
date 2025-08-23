6a7e57e9084dc4e4b001b68b08dcc9ed15cd71a7
Priyanshu <sidarsunita2@gmail.com>
2025-08-24 04:05:09 +0530

    banner javascript markup added

--- src/app/api/webhooks/products/[productId]/banner/route.ts
+++ src/app/api/webhooks/products/[productId]/banner/route.ts
@@ -3,6 +3,10 @@ import { notFound } from "next/navigation";
 import { NextRequest } from "next/server";
 import { env } from "@/data/env/server";
 import { getProductForBanner } from "@/server/db/products";
+import { createProductView } from "@/server/db/productViews";
+import { canRemoveBranding, canShowDiscountBanner } from "@/server/permissions";
+import { createElement } from "react";
+import { Banner } from "@/components/Banner";
 
 export async function GET(
   request: NextRequest,
@@ -21,6 +25,27 @@ export async function GET(
   });
 
   if (product == null) return notFound();
+
+  const canShowBanner = await canShowDiscountBanner(product.clerkUserId);
+
+  await createProductView({
+    productId,
+    countryId: country?.id,
+    userId: product.clerkUserId,
+  });
+
+  if (canShowBanner == null) return notFound();
+  if (country == null || discount == null) return notFound();
+
+  return new Response(
+    await getJavaScript(
+      product,
+      country,
+      discount,
+      await canRemoveBranding(product.clerkUserId)
+    ),
+    { headers: { "content-type": "text/javascript" } }
+  );
 }
 
 function getCountryCode(request: NextRequest) {
@@ -28,4 +53,41 @@ function getCountryCode(request: NextRequest) {
   if (process.env.NODE_ENV === "development") {
     return env.TEST_COUNTRY_CODE;
   }
+}
+
+async function getJavaScript(
+  product: {
+    customization: {
+      classPrefix?: string | null;
+      locationMessage: string;
+      backgroundColor: string;
+      textColor: string;
+      fontSize: string;
+      bannerContainer: string;
+      isSticky: boolean;
+    };
+  },
+  country: { id: string; name: string },
+  discount: { coupon: string; percentage: number },
+  canRemoveBranding: boolean
+) {
+  const { renderToStaticMarkup } = await import("react-dom/server");
+  return `
+    const banner = document.createElement("div");
+    banner.innerHTML = "${renderToStaticMarkup(
+      createElement(Banner, {
+        message: product.customization.locationMessage,
+        mappings: {
+          country: country.name,
+          coupon: discount.coupon,
+          discount: (discount.percentage * 100).toString(),
+        },
+        customization: product.customization,
+        canRemoveBranding,
+      })
+    )}";
+    document.querySelector("${
+      product.customization.bannerContainer
+    }").prepend(...banner.children);
+  `.replace(/(\r\n|\n|\r)/g, "");
 }
